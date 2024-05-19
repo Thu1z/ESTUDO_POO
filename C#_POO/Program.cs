01d6fc4b6f47b94d3a833e4ce65ba662d3e1b069
Thulio <qthulio@gmail.com>
2024-05-19 14:50:49 -0300

    Adicionar arquivos de projeto.

---
+++ C#_POO/Program.cs
@@ -0,0 +1,9 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Paypal";
+musica1.Artista = "Teto";
+musica1.Duracao = 166;
+musica1.Disponivel = true;
+
+musica1.ExibirFichaTecnica();
+Console.WriteLine(musica1.DescricaoResumida);
+
